4e58fbec53f70f4e38fc8edb0b22dbb5537b1a10
clement2987 <109582625+clement2987@users.noreply.github.com>
2024-08-19 14:04:43 +1000

    Update me_graveler.py
    saved a further 5 seconds

--- me_graveler.py
+++ me_graveler.py
@@ -1,13 +1,13 @@
-import random
+from random import random
 import time
 
-start_time = time.time()
+
 
 #total number of attempts the (try to get one billion down to a reasonable time)
 attempts = 1_000_000
 rolls = attempts
 maxOnes = 0
-
+start_time = time.time()
 # Using a for loop because it is faster than a while loop
 for i in range(attempts):
     # takes 0.3 seconds per million attempts
@@ -15,12 +15,11 @@ for i in range(attempts):
         rolls = i
         break
     score = 0
-    for __ in range(231):
-        #takes 10 seconds per million attempts
-        # number = random.random()
-        #check if the number is below 25%. true is 1 and false is 0. this is quicker than an if statement
-        # counting the score takes about 10 seconds per million attempts
-        score += (random.random()<=0.25)
+    for j in range(231):
+       # takes 7 seconds per 1,000,000 attempts
+        if random()<=0.25:
+            score += 1
+        
     #takes about 0.1 seconds per million attempts
     maxOnes += (score * (score > maxOnes)) - (maxOnes * (score > maxOnes))
     
@@ -33,5 +32,5 @@ print("Number of Roll Sessions:",rolls)
 print("time taken:", elapsed_time)
 
 """
-TIME TAKEN FOR 1,000,000 16.467 seconds
+TIME TAKEN FOR 1,000,000 10.267 seconds
 """
